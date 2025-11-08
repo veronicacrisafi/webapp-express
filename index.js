6f1c06257dd14871ed50b440dc3817a3f3ad396a
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-11-08 18:32:28 +0100

    creata prima rotta /  in get di benvenuto al server

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 const connection = require("./database/connections");
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.listen(port, () => {
-  console.log(`In ascolto in http://localhost ${port}`);
+  console.log(`In ascolto in http://localhost:${port}`);
 });
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("Benvenuto nel mio Server!");
+});
