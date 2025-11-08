c1ed337efa8823af59b10184ab5b5f0e22a05b8a
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-11-08 17:59:43 +0100

    creato file connections nella cartella database inserito tutto il colleagamento al db

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Utilizzando il file in allegato, creiamo un database con MySQL Workbench
+Utilizzando il file in allegato, creiamo un database con MySQL Workbench✅
 Creiamo una nuova applicazione Express✅
-Colleghiamo l’app al db e verifichiamo che tutto funzioni
+Colleghiamo l’app al db e verifichiamo che tutto funzioni✅
 Prepariamo una rotta index per ottenere la lista dei film
 Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const mysql = require("mysql2");
 const app = express();
+const connection = require("./database/connections");
 const port = 3000;
 
 app.listen(port, () => {
@@ -8,16 +8,3 @@ app.listen(port, () => {
 });
 
 app.use(express.static("public"));
-
-require("dotenv").config();
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Bravo sei connesso a MySQL Database!");
-});
