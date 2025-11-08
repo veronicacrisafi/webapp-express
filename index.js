2bc5f7751bf97c3e03aff254ca1e6e1f249e052d
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-11-08 17:04:14 +0100

    installato  mysql2 e importato in index.js
    creazione file .env per nascondere i dati privati del progetto
    
    aggiunte le immagini in public per utilizzarle come file statici

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`In ascolto in http://localhost ${port}`);
+});
+
+app.use(express.static("public"));
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
