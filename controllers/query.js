2c6a45d2a5d6732e471583dbc6480ddb177f30d8
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-11-11 22:42:25 +0100

    creata middleware per 404 not found

--- controllers/query.js
+++ controllers/query.js
@@ -14,6 +14,8 @@ function show(req, res) {
   const sql = `SELECT * FROM movies LEFT JOIN reviews ON reviews.movie_id = movies.id WHERE movies.id = ?`;
   connection.query(sql, [id], (err, result) => {
     if (err) return res.status(500).json({ error: "query fallita" });
+    if (result.length === 0)
+      return res.status(404).json({ error: "id query non trovato, riprova!" });
     res.json(result);
   });
 }
